a61555a0af7c09ee79f7c5f2bf21290ebce9111a
0xKubitus <n.cubilier@gmail.com>
2022-06-01 19:54:53 +0200

    Erasing a Task works with both html or JS (AJAX works) = the task is removed from screen without needing to refresh the page ! :)

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -44,7 +44,11 @@ before_action :authenticate_user!
   def destroy
     @task = Task.find(params[:id])
     @task.destroy
-    redirect_to root_path
+    respond_to do |format|
+      format.html { redirect_to books_path }
+      format.js { }
+    end
+    redirect_to root_path, :notice => "The task has been deleted" # just in case, but the redirection is also taken care of by the destroy.js.erb file.
   end
 
 
